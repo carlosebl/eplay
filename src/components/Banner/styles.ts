1aa14730bff9cdebf98fc6ec6c965f59cd5cc535
carlosebl <carlosebl@gmail.com>
2024-04-12 17:07:00 -0300

    iniciada componetização

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { TagContainer } from '../Tag/styles'
 
 export const Imagem = styled.div`
   width: 100%;
@@ -7,6 +8,18 @@ export const Imagem = styled.div`
   background-repeat: no-repeat;
   background-size: cover;
   font-weight: bold;
+
+  .container {
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+  }
+
+  ${TagContainer} {
+    position: absolute;
+    top: 32px;
+  }
 `
 
 export const Titulo = styled.h2`
