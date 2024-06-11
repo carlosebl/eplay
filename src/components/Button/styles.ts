4ceac61e0360b19871eec3858b64b53e1480afa6
Carlos Leite <carlosebl@gmail.com>
2024-06-11 13:45:42 -0300

    finalizado módulo 36

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,11 +1,14 @@
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { cores } from '../../styles'
+import { Props } from '.'
 
-export const ButtonContainer = styled.button`
-  border: 2px solid ${cores.branca};
+export const ButtonContainer = styled.button<Props>`
+  border: 2px solid
+    ${(props) => (props.variant === 'primary' ? cores.verde : cores.branca)};
   border-radius: 8px;
-  background-color: transparent;
+  background-color: ${(props) =>
+    props.variant === 'primary' ? cores.verde : 'tranparent'};
   color: ${cores.branca};
   font-size: 16px;
   font-weight: bold;
